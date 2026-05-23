cd13824d1f0f2446896a0dc69749a8164f8a7389
ash_701 <ashmithrao1729@gmail.com>
2026-05-23 12:04:43 +0530

    sliding.py

--- sliding.py
+++ sliding.py
@@ -1,23 +1,13 @@
-def sliding(list,m,n):
-    l=len(list)-n
-    i=0
-    s=[]
-    while i<=l:
-        j=0
-        sum=0
-        f=[]
-        k=i
-        while j<=n-1:
-            sum=sum+list[k]
-            f.append(list[k])
-            k+=1
-            j+=1    
-            
+def sliding(arr, m, n):
+    s = []
+    sum = sum(arr[:n])
+    if sum == m:
+        s.append(arr[:n])
+    i=1
+    for i in range(len(arr)-n):
+        sum=sum-arr[i-1]+arr[i+n-1]
         if sum==m:
-            s.append(f)
-        i+=1
+            s.append(arr[i:i+n])
     return s
-list=[2, 1, 5, 1, 3, 2, 4, 3]
-n=3
-m=9
-print(sliding(list,m,n))
+arr = [5,2,-1,0,3]
+print(sliding(arr, 6, 3))
